77b1e791a72c764be761a3a31f5875597f1507c4
Dukeeeee <40759437+GithubDuke@users.noreply.github.com>
2020-05-31 13:01:40 +0200

    Fixed the generated files

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:salarix/app/locator.dart';
 import 'package:salarix/app/router.gr.dart';
 
 void main() {
+  setupLocator();
   runApp(MyApp());
 }
 
