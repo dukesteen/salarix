ca14e1a9a70ce5fac85cc2ae71b517b37c62ac90
Dukeeeee <40759437+GithubDuke@users.noreply.github.com>
2020-06-02 12:29:46 +0200

    Added functionality for adding salary entries.

--- lib/ui/views/add_salary_entry/add_salary_entry_viewmodel.dart
+++ lib/ui/views/add_salary_entry/add_salary_entry_viewmodel.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:salarix/app/locator.dart';
 import 'package:salarix/models/salary_model.dart';
 import 'package:salarix/services/salary_service.dart';
@@ -7,6 +8,10 @@ import 'package:stacked_services/stacked_services.dart';
 class AddSalaryEntryViewModel extends ReactiveViewModel {
   final NavigationService _navigationService = locator<NavigationService>();
   final SalaryService _salaryService = locator<SalaryService>();
+  final DialogService _dialogService = locator<DialogService>();
+
+  final hoursWorkedController = TextEditingController();
+  final hourlyWageController = TextEditingController();
 
   SalaryModel _salaryModel = SalaryModel(dateWorked: DateTime.now());
   SalaryModel get salaryModel => _salaryModel;
@@ -21,5 +26,39 @@ class AddSalaryEntryViewModel extends ReactiveViewModel {
 
   void navBack() {
     _navigationService.back();
+  }
+
+  void initModel() {
+    print('AddSalaryEntryViewModel initialised');
+    hoursWorkedController.addListener(_hoursWorkedUpdate);
+    hourlyWageController.addListener(_hourlyWageUpdate);
+    notifyListeners();
+  }
+
+  void _hoursWorkedUpdate() {
+    _salaryModel.hoursWorked =
+        num.tryParse(hoursWorkedController.text)?.toDouble();
+    notifyListeners();
+  }
+
+  void _hourlyWageUpdate() {
+    salaryModel.hourlyWage =
+        num.tryParse(hourlyWageController.text)?.toDouble();
+    notifyListeners();
+  }
+
+  void submitModel() {
+    if (salaryModel.hoursWorked == null ||
+        salaryModel.hourlyWage == null ||
+        salaryModel.dateWorked == null) {
+      _dialogService.showDialog(
+        title: "Error occured",
+        description: "Please fill out the entire form.",
+        buttonTitle: "OK",
+      );
+    } else {
+      _salaryService.add(salaryModel);
+      navBack();
+    }
   }
 }
